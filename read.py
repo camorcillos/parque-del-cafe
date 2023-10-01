1ed53368028e5c7824b8107fd0feb057e9ea066e
camorcillos <camorcillos@gmail.com>
2023-10-01 12:22:14 -0500

    Escribiendo en Sheets

--- read.py
+++ read.py
@@ -36,68 +36,42 @@ def main():
     values_input = result_input.get('values', [])
     print(values_input)
 
-    # if not values_input and not values_expansion:
-    #     print('No data found.')
+        # if not values_input and not values_expansion:
+        #     print('No data found.')
+
+
+    # df = pd.DataFrame(values_input, columns=values_input[0])
+    df = pd.DataFrame(values_input)
+    df = df.fillna(0)
+
+
+    # df.to_excel('./excel.xlsx', index=False, header=False)
+
+ # Verifica si values_input no está vacío (ya existe data)
+    if values_input:
+        # Crea una nueva fila con los valores deseados
+        new_row_values = ["New Value 1", "New Value 2", "New Value 3", "New Value 4", "New Value 5", "New Value 6", "New Value 7", "New Value 8"]  # Reemplaza con tus valores reales
+
+        # Encuentra la última fila no vacía en la hoja de cálculo
+        last_row = len(values_input) + 1
+
+        # Construye el nuevo rango para agregar la fila sin espacio
+        new_range = f'A{last_row}:H{last_row}'
+
+        # Actualiza los valores en la hoja de cálculo
+        response = sheet.values().update(
+            spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+            range=new_range,
+            valueInputOption='RAW',
+            body={'values': [new_row_values]}
+        ).execute()
+
+        print(f'Nueva fila agregada con respuesta: {response}')
 
 main()
 
-# df = pd.DataFrame(values_input, columns=values_input[0])
-df = pd.DataFrame(values_input)
-df = df.fillna(0)
 
 
-# df.to_excel('./excel.xlsx', index=False, header=False)
 
 
-df_gold=df[(df['Medal']=='Gold') & (df['Sport']=='Gymnastics')]
 
-#change this by your sheet ID
-SAMPLE_SPREADSHEET_ID_input = '189UzdG5vAlmprVxlSmewp-T7vfPfUPl6vIyWf4SR5x'
-
-#change the range if needed
-SAMPLE_RANGE_NAME = 'A1:AA1000'
-
-def Create_Service(client_secret_file, api_service_name, api_version, *scopes):
-    global service
-    SCOPES = [scope for scope in scopes[0]]
-    #print(SCOPES)
-    
-    cred = None
-
-    if os.path.exists('token_write.pickle'):
-        with open('token_write.pickle', 'rb') as token:
-            cred = pickle.load(token)
-
-    if not cred or not cred.valid:
-        if cred and cred.expired and cred.refresh_token:
-            cred.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(client_secret_file, SCOPES)
-            cred = flow.run_local_server()
-
-        with open('token_write.pickle', 'wb') as token:
-            pickle.dump(cred, token)
-
-    try:
-        service = build(api_service_name, api_version, credentials=cred)
-        print(api_service_name, 'service created successfully')
-        #return service
-    except Exception as e:
-        print(e)
-        #return None
-        
-# change 'my_json_file.json' by your downloaded JSON file.
-Create_Service('credentials.json', 'sheets', 'v4',['https://www.googleapis.com/auth/spreadsheets'])
-    
-def Export_Data_To_Sheets():
-    response_date = service.spreadsheets().values().update(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
-        valueInputOption='RAW',
-        range=SAMPLE_RANGE_NAME,
-        body=dict(
-            majorDimension='ROWS',
-            values=df_gold.T.reset_index().T.values.tolist())
-    ).execute()
-    print('Sheet successfully Updated')
-
-Export_Data_To_Sheets()
